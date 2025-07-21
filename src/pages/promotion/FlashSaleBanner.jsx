f958a78f5268ba855b50ad9b99870b7fe3dcacd5
ChangPe-lala <ntrang2110@gmail.com>
2025-07-22 02:18:21 +0700

    update Promotion

--- src/pages/promotion/FlashSaleBanner.jsx
+++ src/pages/promotion/FlashSaleBanner.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import banner from "../../icons/Group37103.png"; // ảnh banner của bạn
+import banner from "../../icons/Banner_P.png";
 import flower from "../../icons/flower.png";
 import { useNavigate } from "react-router-dom";
 
@@ -28,13 +28,13 @@ const FlashSaleBanner = () => {
 
       {/* Timer */}
 
-      <div className="flex items-center font-genos text-[45px] font-extrabold tracking-[.6em] text-black absolute top-[317px] left-[70%] transform -translate-x-1/2  rotate-[-3deg]">
+      <div className="flex items-center font-genos text-[47px] font-extrabold tracking-[.7em] text-black absolute top-[325px] left-[78%] transform -translate-x-1/2  rotate-[-3deg]">
         <span className="ml-6">{formatTime(timeLeft)}</span>
       </div>
 
       <button
         onClick={() => navigate("/shop")}
-        className="absolute top-[250px] left-[815px] flex items-center gap-2 bg-black text-white px-5 py-3 rounded-[15px] text-[18px] font-bold shadow-lg hover:bg-white hover:text-black transition max-w-[160px]"
+        className="absolute top-[240px] left-[870px] flex items-center gap-2 bg-black text-white px-5 py-3 rounded-[15px] text-[20px] font-bold shadow-lg hover:bg-white hover:text-black transition max-w-[160px]"
       >
         <img src={flower} alt="flower" className="w-5 h-5 flex-shrink-0" />
         <span className="break-words leading-tight text-center">
